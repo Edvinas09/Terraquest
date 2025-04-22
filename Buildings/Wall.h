da6924f29ea54f6ed074ca82f17be09dd0f2bfcc
Zalciukas <augustinas.punis@mif.stud.vu.lt>
2025-04-22 20:04:31 +0300

    Create Wall.h

---
+++ Buildings/Wall.h
@@ -0,0 +1,23 @@
+#ifndef WALL_H
+#define WALL_H
+
+#include "Building.h"
+
+// Wall class - defensive structure
+class Wall : public Building {
+private:
+    int defenseBonus;
+    int level;
+    
+public:
+    Wall(Player* owner, Tile* location, int level = 1);
+    
+    int getDefenseBonus() const;
+    int getLevel() const;
+    
+    void upgrade();
+    void update(int turn) override;
+    std::string getDescription() const override;
+};
+
+#endif // WALL_H
