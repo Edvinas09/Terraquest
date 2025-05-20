4dfa7941d0462eb962e77fab2641aa2ff56bc6a2
Edvinas09 <edvinas.bagdonas10@gmail.com>
2025-05-20 17:21:46 +0300

    Revert "Moved logic to a new file"
    This reverts commit 947a4ec89e1db4d4f88d1777e4eb6ffca584eb4b.

--- EventHandler.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "imgui.h"
-#include "imgui-SFML.h"
-
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-
-void run(sf::RenderWindow& window, const std::optional<sf::Event> event); 
